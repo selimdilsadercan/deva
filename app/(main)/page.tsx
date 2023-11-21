ac1a3d2094d81daad7247b820fdaa0a6f80f2c25
selimdilsad <selimdilsadercan@gmail.com>
2023-11-21 22:54:59 +0300

    deleted uygulamalr component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,22 +1,12 @@
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger
-} from "@/components/ui/tabs";
-import Uygulamalar from "@/components/uygulamalar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Page() {
   return (
     <div className="flex flex-col h-full">
       <Tabs defaultValue="uygulamalar">
         <TabsList className="grid w-full md:w-[400px] grid-cols-2 mb-4">
-          <TabsTrigger value="aboneliklerim">
-            Aboneliklerim
-          </TabsTrigger>
-          <TabsTrigger value="uygulamalar">
-            Tüm Uygulamalar
-          </TabsTrigger>
+          <TabsTrigger value="aboneliklerim">Aboneliklerim</TabsTrigger>
+          <TabsTrigger value="uygulamalar">Tüm Uygulamalar</TabsTrigger>
         </TabsList>
 
         <TabsContent value="aboneliklerim">
@@ -26,9 +16,7 @@ function Page() {
           ))} */}
         </TabsContent>
 
-        <TabsContent value="uygulamalar">
-          <Uygulamalar />
-        </TabsContent>
+        <TabsContent value="uygulamalar"></TabsContent>
       </Tabs>
     </div>
   );
